c5a02638142cf6b5bd5dc82b9d1ded81bfe66923
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-11-26 20:16:26 +0900

    Add blink(1) manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Installs Blink(1) Control
+#
+# Examples:
+#
+#    include blink1
+
+class blink1 {
+  package { 'blink1':
+    source   => 'http://thingm.com/blink1/downloads/Blink1Control-mac.zip',
+    provider => 'compressed_app'
+  }
 }
