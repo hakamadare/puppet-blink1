329d2183372722e114fca5449618d4211f0f31c7
Steve Huff <steve.huff@runkeeper.com>
2014-06-10 13:07:41 -0400

    fixing application name

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #    include blink1
 
 class blink1 {
-  package { 'blink1':
+  package { 'Blink1Control.app':
     source   => 'http://thingm.com/blink1/downloads/Blink1Control-mac.zip',
     provider => 'compressed_app'
   }
