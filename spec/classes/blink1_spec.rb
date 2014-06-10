b634ccda15550dabc388d2902c0c24d8b09e554b
Steve Huff <steve.huff@runkeeper.com>
2014-06-10 13:36:05 -0400

    fixing unit test

--- spec/classes/blink1_spec.rb
+++ spec/classes/blink1_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe 'blink1' do
   it { should include_class('blink1') }
-  it { should contain_package('blink1') }
+  it { should contain_package('Blink1Control') }
 end
