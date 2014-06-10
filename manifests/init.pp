b7bfce4aec177fe14406ae314ab410064f4e8e7c
Steve Huff <steve.huff@runkeeper.com>
2014-06-10 13:33:14 -0400

    change download location, add $version param

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,9 +4,11 @@
 #
 #    include blink1
 
-class blink1 {
+class blink1 (
+  $version = '1.7',
+) {
   package { 'Blink1Control':
-    source   => 'http://thingm.com/blink1/downloads/Blink1Control-mac.zip',
+    source   => "https://github.com/todbot/blink1/releases/download/v${version}/Blink1Control-mac.zip",
     provider => 'compressed_app'
   }
 }
